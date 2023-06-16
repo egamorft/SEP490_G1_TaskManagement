bab5f3c156739dfb4ca155341ad1e2279c4a893a
egamorft <huytung.htbn@gmail.com>
2023-06-16 22:38:43 +0700

    Set svg logo as FTask at sidebar, navbar

--- resources/views/content/authentication/auth-verify-email-cover.blade.php
+++ resources/views/content/authentication/auth-verify-email-cover.blade.php
@@ -1,70 +1,86 @@
 @php
-$configData = Helper::applClasses();
+    $configData = Helper::applClasses();
 @endphp
 @extends('layouts/fullLayoutMaster')
 
 @section('title', 'Verify Email Cover')
 
 @section('page-style')
-  <link rel="stylesheet" href="{{ asset(mix('css/base/pages/authentication.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/base/pages/authentication.css')) }}">
 @endsection
 
 @section('content')
-<div class="auth-wrapper auth-cover">
-  <div class="auth-inner row m-0">
-    <!-- Brand logo-->
-    <a class="brand-logo" href="#">
-      <svg viewbox="0 0 139 95" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="28">
-        <defs>
-          <lineargradient id="linearGradient-1" x1="100%" y1="10.5120544%" x2="50%" y2="89.4879456%">
-            <stop stop-color="#000000" offset="0%"></stop>
-            <stop stop-color="#FFFFFF" offset="100%"></stop>
-          </lineargradient>
-          <lineargradient id="linearGradient-2" x1="64.0437835%" y1="46.3276743%" x2="37.373316%" y2="100%">
-            <stop stop-color="#EEEEEE" stop-opacity="0" offset="0%"></stop>
-            <stop stop-color="#FFFFFF" offset="100%"></stop>
-          </lineargradient>
-        </defs>
-        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-          <g id="Artboard" transform="translate(-400.000000, -178.000000)">
-            <g id="Group" transform="translate(400.000000, 178.000000)">
-              <path class="text-primary" id="Path" d="M-5.68434189e-14,2.84217094e-14 L39.1816085,2.84217094e-14 L69.3453773,32.2519224 L101.428699,2.84217094e-14 L138.784583,2.84217094e-14 L138.784199,29.8015838 C137.958931,37.3510206 135.784352,42.5567762 132.260463,45.4188507 C128.736573,48.2809251 112.33867,64.5239941 83.0667527,94.1480575 L56.2750821,94.1480575 L6.71554594,44.4188507 C2.46876683,39.9813776 0.345377275,35.1089553 0.345377275,29.8015838 C0.345377275,24.4942122 0.230251516,14.560351 -5.68434189e-14,2.84217094e-14 Z" style="fill: currentColor"></path>
-              <path id="Path1" d="M69.3453773,32.2519224 L101.428699,1.42108547e-14 L138.784583,1.42108547e-14 L138.784199,29.8015838 C137.958931,37.3510206 135.784352,42.5567762 132.260463,45.4188507 C128.736573,48.2809251 112.33867,64.5239941 83.0667527,94.1480575 L56.2750821,94.1480575 L32.8435758,70.5039241 L69.3453773,32.2519224 Z" fill="url(#linearGradient-1)" opacity="0.2"></path>
-              <polygon id="Path-2" fill="#000000" opacity="0.049999997" points="69.3922914 32.4202615 32.8435758 70.5039241 54.0490008 16.1851325"></polygon>
-              <polygon id="Path-21" fill="#000000" opacity="0.099999994" points="69.3922914 32.4202615 32.8435758 70.5039241 58.3683556 20.7402338"></polygon>
-              <polygon id="Path-3" fill="url(#linearGradient-2)" opacity="0.099999994" points="101.428699 0 83.0667527 94.1480575 130.378721 47.0740288"></polygon>
-            </g>
-          </g>
-        </g>
-      </svg>
-      <h2 class="brand-text text-primary ms-1">Vuexy</h2>
-    </a>
-    <!-- /Brand logo-->
+    <div class="auth-wrapper auth-cover">
+        <div class="auth-inner row m-0">
+            <!-- Brand logo-->
+            <a class="brand-logo" href="#">
+                <svg width="75" height="53" viewBox="0 0 45 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g clip-path="url(#clip0_104_129874)">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M41 15.6875L40.5 17.6892C40.5 17.6892 40.5 21.9506 40.5 23.1875H41.2774L41.7218 21.6875L42.5 23.1875H45L41.2774 20.0613L44 18.6872L44.5 16.626L41.2774 18.6872L41.7218 16.626L41 15.6875Z"
+                            fill="#4267B2" />
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M36.5 15.1875L36 17.1875L36.5 20.1875C36.5 20.1875 38.2778 19.7232 38.2778 20.9601V22.1875L36 21.6875L36.5 23.1875H39L40 21.1875V20.0613L38.2778 18.7812L37.2774 18.6872L37.7218 16.626L38.608 17.6892L40 16.6875L38.6111 15.1875H36.5Z"
+                            fill="#4267B2" />
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M32 15.6875L31.5 17.6892C31.5 17.6892 31.5 21.9506 31.5 23.1875H32.2774V20.0613H33.608L33.2778 18.7812L32.2774 18.6872L32.7218 16.626L34.3889 23.0886H35.5L33.6111 15.1875L32 15.6875Z"
+                            fill="#4267B2" />
+                        <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd"
+                            d="M11.9119 19.3717L0.661865 22.8156H35.7354H59.5589L45.0001 19.3717H11.9119Z" fill="#161616" />
+                        <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd"
+                            d="M19.1912 15.9279V17.8958L23.5147 17.2398L28.4559 17.8958V17.2398V15.9279L23.5147 16.5838L19.1912 15.9279Z"
+                            fill="#161616" />
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M0.5 0V7.1923C0.5 7.1923 0.5 19.444 0.5 23H6.98676L3.74259 14.0121L15.7924 13.5182L16.7433 10.0615H3.74259L5.5964 4.13572L16.7433 7.1923L19.5 0.185181L0.5 0Z"
+                            fill="url(#paint0_linear_104_129874)" />
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M24.5 18.1875L25.2273 7.1875H20.5L21.5909 3.6875L36.5 0.1875L34.6818 7.1875H28.5C28.5 7.1875 27.0455 9.1875 28.5 12.1875L29.2273 23.1875H25.4091H21.5909L24.5 18.1875Z"
+                            fill="#28C76F" />
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M24.5 18.1875L25.2273 7.1875H20.5L21.5909 3.6875L36.5 0.1875L34.6818 7.1875H28.5C28.5 7.1875 27.0455 9.1875 28.5 12.1875L29.2273 23.1875H25.4091H21.5909L24.5 18.1875Z"
+                            fill="black" fill-opacity="0.25" />
+                    </g>
+                    <defs>
+                        <linearGradient id="paint0_linear_104_129874" x1="0.5" y1="23" x2="23.0865"
+                            y2="4.34157" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#FF9F43" />
+                            <stop offset="1" stop-color="#FFB976" />
+                        </linearGradient>
+                        <clipPath id="clip0_104_129874">
+                            <rect width="45" height="23" fill="white" />
+                        </clipPath>
+                    </defs>
+                </svg>
+            </a>
+            <!-- /Brand logo-->
 
-    <!-- Left Text-->
-    <div class="d-none d-lg-flex col-lg-8 align-items-center p-5">
-      <div class="w-100 d-lg-flex align-items-center justify-content-center px-5">
-        @if($configData['theme'] === 'dark')
-         <img src="{{asset('images/illustration/verify-email-illustration-dark.svg')}}" class="img-fluid" alt="two steps verification" />
-        @else
-        <img src="{{asset('images/illustration/verify-email-illustration.svg')}}" class="img-fluid" alt="two steps verification" />
-        @endif
+            <!-- Left Text-->
+            <div class="d-none d-lg-flex col-lg-8 align-items-center p-5">
+                <div class="w-100 d-lg-flex align-items-center justify-content-center px-5">
+                    @if ($configData['theme'] === 'dark')
+                        <img src="{{ asset('images/illustration/verify-email-illustration-dark.svg') }}" class="img-fluid"
+                            alt="two steps verification" />
+                    @else
+                        <img src="{{ asset('images/illustration/verify-email-illustration.svg') }}" class="img-fluid"
+                            alt="two steps verification" />
+                    @endif
+                </div>
+            </div>
+            <!-- /Left Text-->
+
+            <!-- verify email cover-->
+            <div class="d-flex col-lg-4 align-items-center auth-bg px-2 p-lg-5">
+                <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
+                    <h2 class="card-title fw-bolder mb-1">Verify your email &#x2709;&#xFE0F;</h2>
+                    <p class="card-text mb-2">Account activation link sent to your email address:<span class="fw-bolder">
+                            hello@pixinvent.com</span> Please follow the link inside to continue.</p>
+                    <a class="btn btn-primary w-100" href="{{ asset('/') }}">Skip for now</a>
+                    <p class="text-center mt-2"><span>Didn&apos;t receive an email?</span>
+                        <a href="Javascript:void(0)"><span>&nbsp;Resend</span></a>
+                    </p>
+                </div>
+            </div>
+            <!-- verify email cover-->
         </div>
     </div>
-    <!-- /Left Text-->
-
-    <!-- verify email cover-->
-    <div class="d-flex col-lg-4 align-items-center auth-bg px-2 p-lg-5">
-      <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
-        <h2 class="card-title fw-bolder mb-1">Verify your email &#x2709;&#xFE0F;</h2>
-        <p class="card-text mb-2">Account activation link sent to your email address:<span class="fw-bolder"> hello@pixinvent.com</span> Please follow the link inside to continue.</p>
-        <a class="btn btn-primary w-100" href="{{asset('/')}}">Skip for now</a>
-        <p class="text-center mt-2"><span>Didn&apos;t receive an email?</span>
-            <a href="Javascript:void(0)"><span>&nbsp;Resend</span></a>
-        </p>
-      </div>
-    </div>
-    <!-- verify email cover-->
-  </div>
-</div>
 @endsection
