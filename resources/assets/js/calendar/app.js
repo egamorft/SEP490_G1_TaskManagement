96455c56a37cb2bb3e5045947fe8f4515aa9298c
vietphan <vietpthe150767@fpt.edu.vn>
2023-06-26 22:36:49 +0700

    Sidebar

---
+++ resources/assets/js/calendar/app.js
@@ -0,0 +1,22 @@
+var user_buttons = {
+    "all": {
+        color: "",
+        content: "All",
+        value: "all"
+    },
+    "creator": {
+        color: "",
+        content: "Creator",
+        value: "creator"
+    },
+    "assignee": {
+        color: "",
+        content: "Assignee",
+        value: "assignee"
+    },
+    "reviewer": {
+        color: "",
+        content: "Reviewer",
+        value: "reviewer"
+    }
+};
--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -106,35 +106,44 @@
                             {{-- USER role --}}
                             @if (isset($menu->navheader))
                                     <li class="navigation-header">
-                                        <span>{{ __('locale.' . $menu->navheader) }}</span>
+                                        <span>{{ __($menu->navheader) }}</span>
                                         <i data-feather="more-horizontal"></i>
                                     </li>
                                 @else
-                                    {{-- Add Custom Class with nav-item --}}
-                                    @php
-                                        $custom_classes = '';
-                                        if (isset($menu->classlist)) {
-                                            $custom_classes = $menu->classlist;
-                                        }
-                                    @endphp
-                                    <li
-                                        class="nav-item {{ $custom_classes }} {{ Route::currentRouteName() === $menu->slug ? 'active' : '' }}">
-                                        <a href="{{ isset($menu->url) ? url($menu->url) : 'javascript:void(0)' }}"
-                                            class="d-flex align-items-center"
-                                            target="{{ isset($menu->newTab) ? '_blank' : '_self' }}">
-                                            <i data-feather="{{ $menu->icon }}"></i>
-                                            <span
-                                                class="menu-title text-truncate">{{ __('locale.' . $menu->name) }}</span>
-                                            @if (isset($menu->badge))
-                                                <?php $badgeClasses = 'badge rounded-pill badge-light-primary ms-auto me-1'; ?>
+                                    @if (isset($menu->navitems))
+                                        <li
+                                            class="nav-item {{ $menu->class }}" id="{{ $menu->id }}">
+                                            <span class="my-project-items menu-title text-truncate">
+                                                {{ $menu->html }}
+                                            </span>
+                                        </li>
+                                    @else
+                                        {{-- Add Custom Class with nav-item --}}
+                                        @php
+                                            $custom_classes = '';
+                                            if (isset($menu->classlist)) {
+                                                $custom_classes = $menu->classlist;
+                                            }
+                                        @endphp
+                                        <li
+                                            class="nav-item {{ $custom_classes }} {{ Route::currentRouteName() === $menu->slug ? 'active' : '' }}">
+                                            <a href="{{ isset($menu->url) ? url($menu->url) : 'javascript:void(0)' }}"
+                                                class="d-flex align-items-center"
+                                                target="{{ isset($menu->newTab) ? '_blank' : '_self' }}">
+                                                <i data-feather="{{ isset($menu->icon) ? $menu->icon : '' }}"></i>
                                                 <span
-                                                    class="{{ isset($menu->badgeClass) ? $menu->badgeClass : $badgeClasses }}">{{ $menu->badge }}</span>
+                                                    class="menu-title text-truncate">{{ __(isset($menu->name) ? $menu->name : '') }}</span>
+                                                @if (isset($menu->badge))
+                                                    <?php $badgeClasses = 'badge rounded-pill badge-light-primary ms-auto me-1'; ?>
+                                                    <span
+                                                        class="{{ isset($menu->badgeClass) ? $menu->badgeClass : $badgeClasses }}">{{ $menu->badge }}</span>
+                                                @endif
+                                            </a>
+                                            @if (isset($menu->submenu))
+                                                @include('panels/submenu', ['menu' => $menu->submenu])
                                             @endif
-                                        </a>
-                                        @if (isset($menu->submenu))
-                                            @include('panels/submenu', ['menu' => $menu->submenu])
-                                        @endif
-                                    </li>
+                                        </li>
+                                    @endif
                                 @endif
                         @endif
 
