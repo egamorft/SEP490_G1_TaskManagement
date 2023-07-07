792f53778e4045425e9ab3313234fe6a94b133b8
vietphan <vietpthe150767@fpt.edu.vn>
2023-07-07 23:44:58 +0700

    Fix filter

--- resources/js/scripts/pages/task-filter.js
+++ resources/js/scripts/pages/task-filter.js
@@ -1,5 +1,5 @@
 /*=========================================================================================
-    File Name: task-list.js
+    File Name: dashboard-ecommerce.js
     Description: dashboard ecommerce page content with Apexchart Examples
     ----------------------------------------------------------------------------------------
     Item Name: Vuexy  - Vuejs, HTML & Laravel Admin Dashboard Template
