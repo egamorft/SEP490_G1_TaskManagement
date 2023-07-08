cd1d28eb8f6e3dbd04c725c3d8c9412fa939f204
Trần Ngọc Hiếu <hieutnhe15199@fpt.edu.vn>
2023-07-08 14:11:37 +0700

    update change status

--- resources/js/scripts/pages/task-change-status-assignee-reviewer.js
+++ resources/js/scripts/pages/task-change-status-assignee-reviewer.js
@@ -10,7 +10,8 @@
 $(window).on("load", function () {
     "use strict";
     var addAssignee = $(".add-assignee"),
-        addReviewer = $(".add-reviewer");
+        addReviewer = $(".add-reviewer"),
+		changeStatus = $(".change-task-status");
 
 	function assignTask(acc_id) {
 		var currentUrl = window.location.href;
@@ -107,6 +108,53 @@ $(window).on("load", function () {
 		});
 	}
 
+	function changeTaskStatus(status) {
+		var currentUrl = window.location.href;
+		var task_id = currentUrl.split('/').pop();
+		var csrfToken = $('input[name="csrf-token"]').val();
+		var isRtl = $('html').attr('data-textdirection') === 'rtl';
+
+		console.log(status);
+		$.ajax({
+			url: "change-status",
+			method: "POST",
+			headers: {
+				'X-CSRF-TOKEN': csrfToken  // Include the CSRF token in the headers
+			  },
+			data: {
+				task_id: task_id,
+				status: status,
+			},
+			success: function (response) {
+				// Handle success response
+				setTimeout(function () {
+					toastr["success"](response.message, "Success!", {
+						showMethod: "slideDown",
+						hideMethod: "slideUp",
+						progressBar: true,
+						closeButton: true,
+						tapToDismiss: false,
+						rtl: isRtl,
+					});
+				}, 2000);
+				// window.location.reload();
+			},
+			error: function (response) {
+				// Handle error response
+				setTimeout(function () {
+					toastr["error"](response.message, "Error!", {
+						showMethod: "slideDown",
+						hideMethod: "slideUp",
+						progressBar: true,
+						closeButton: true,
+						tapToDismiss: false,
+						rtl: isRtl,
+					});
+				}, 2000);
+			},
+		});
+	}
+
 	addAssignee.on('click', function () {
 		var acc_id = $(this).attr('id').split('_')[0];
 		assignTask(acc_id);
@@ -115,5 +163,10 @@ $(window).on("load", function () {
 	addReviewer.on('click', function () {
 		var acc_id = $(this).attr('id').split('_')[0];
 		assignTaskReview(acc_id);
+	});
+
+	changeStatus.on('click', function () {
+		var status = $(this).attr('id').split('_')[0];
+		changeTaskStatus(status);
 	});
 });
