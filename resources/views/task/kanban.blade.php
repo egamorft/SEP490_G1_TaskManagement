ae10f8f1ee94612b03fbe925704fd6310d680587
vietphan <vietpthe150767@fpt.edu.vn>
2023-07-09 17:18:47 +0700

    Update form

--- resources/views/task/kanban.blade.php
+++ resources/views/task/kanban.blade.php
@@ -1,235 +1,107 @@
 <!-- Kanban starts -->
 <section class="app-kanban-wrapper">
-	<div class="row">
-	  <div class="col-12">
-		<form class="add-new-board">
-		  <label class="add-new-btn mb-2" for="add-new-board-input">
-			<i class="align-middle" data-feather="plus"></i>
-			<span class="align-middle">Add new</span>
-		  </label>
-		  <input
-			type="text"
-			class="form-control add-new-board-input mb-50"
-			placeholder="Add Board Title"
-			id="add-new-board-input"
-			required
-		  />
-		  <div class="mb-1 add-new-board-input">
-			<button class="btn btn-primary btn-sm me-75">Add</button>
-			<button type="button" class="btn btn-outline-secondary btn-sm cancel-add-new">Cancel</button>
-		  </div>
-		</form>
-	  </div>
-	</div>
-	<!-- Kanban content starts -->
-	<div class="kanban-wrapper"></div>
-	<!-- Kanban content ends -->
-	<!-- Kanban Sidebar starts -->
-	<div class="modal modal-slide-in update-item-sidebar fade">
-	  <div class="modal-dialog sidebar-lg">
-		<div class="modal-content p-0">
-		  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
-		  <div class="modal-header mb-1">
-			<h5 class="modal-title">Update Item</h5>
-		  </div>
-		  <div class="modal-body flex-grow-1">
-			<ul class="nav nav-tabs tabs-line">
-			  <li class="nav-item">
-				<a class="nav-link nav-link-update active" data-bs-toggle="tab" href="#tab-update">
-				  <i data-feather="edit"></i>
-				  <span class="align-middle">Update</span>
-				</a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link nav-link-activity" data-bs-toggle="tab" href="#tab-activity">
-				  <i data-feather="activity"></i>
-				  <span class="align-middle">Activity</span>
-				</a>
-			  </li>
-			</ul>
-			<div class="tab-content mt-2">
-			  <div class="tab-pane tab-pane-update fade show active" id="tab-update" role="tabpanel">
-				<form class="update-item-form">
-				  <div class="mb-1">
-					<label class="form-label" for="title">Title</label>
-					<input type="text" id="title" class="form-control" placeholder="Enter Title" />
-				  </div>
-				  <div class="mb-1">
-					<label class="form-label" for="due-date">Due Date</label>
-					<input type="text" id="due-date" class="form-control" placeholder="Enter Due Date" />
-				  </div>
-				  <div class="mb-1">
-					<label class="form-label" for="label">Label</label>
-					<select class="select2 select2-label form-select" id="label">
-					  <option value="">&nbsp;</option>
-					  <option data-color="badge-light-success" value="UX">UX</option>
-					  <option data-color="badge-light-warning" value="Images">Images</option>
-					  <option data-color="badge-light-info" value="App">App</option>
-					  <option data-color="badge-light-danger" value="Code Review">Code Review</option>
-					  <option data-color="badge-light-success" value="Forms">Forms</option>
-					  <option data-color="badge-light-primary" value="Charts & Maps">Charts & Maps</option>
-					</select>
-				  </div>
-				  <div class="mb-1">
-					<label class="form-label">Assigned</label>
-					<ul class="assigned ps-0"></ul>
-				  </div>
-				  <div class="mb-1">
-					<label for="attachments" class="form-label">Attachments</label>
-					<input class="form-control file-attachments" type="file" id="attachments" multiple />
-				  </div>
-				  <div class="mb-1">
-					<label class="form-label">Comment</label>
-					<div class="comment-editor border-bottom-0"></div>
-					<div class="d-flex justify-content-end comment-toolbar">
-					  <span class="ql-formats me-0">
-						<button class="ql-bold"></button>
-						<button class="ql-italic"></button>
-						<button class="ql-underline"></button>
-						<button class="ql-link"></button>
-						<button class="ql-image"></button>
-					  </span>
+    <div class="row">
+        <div class="col-12">
+            <form class="add-new-board">
+                <label class="add-new-btn mb-2" for="add-new-board-input">
+                    <i class="align-middle" data-feather="plus"></i>
+                    <span class="align-middle">Add new</span>
+                </label>
+                <input type="text" class="form-control add-new-board-input mb-50" placeholder="Add Board Title"
+                    id="add-new-board-input" required />
+                <div class="mb-1 add-new-board-input">
+                    <button class="btn btn-primary btn-sm me-75">Add</button>
+                    <button type="button" class="btn btn-outline-secondary btn-sm cancel-add-new">Cancel</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- Kanban content starts -->
+    <div class="kanban-wrapper"></div>
+    <!-- Kanban content ends -->
+    <!-- Kanban Sidebar starts -->
+    <div class="modal update-item-sidebar fade" id="addNewProject" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered modal-edit-user">
+            <div class="modal-content">
+                <div class="modal-header bg-transparent">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body pb-5 px-sm-3 pt-50">
+                    <div class="mb-2 kanban-detail-header">
+                        <div class="mb-1 kanban-detail-title">Code màn chức năng view BE</div>
+						<div class="mb-1 kanban-detail-status">
+							Trong danh sách <span><u>Đang làm</u></span>
+						</div>
 					</div>
-				  </div>
-				  <div class="mb-1">
-					<div class="d-flex flex-wrap">
-					  <button class="btn btn-primary me-1" data-bs-dismiss="modal">Save</button>
-					  <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Delete</button>
+
+					<div class="mb-2 kanban-detail-progress">
+						<div class="mb-1">
+							<div class="">
+
+							</div>
+						</div>
 					</div>
-				  </div>
-				</form>
-			  </div>
-			  <div class="tab-pane tab-pane-activity pb-1 fade" id="tab-activity" role="tabpanel">
-				<div class="d-flex align-items-start mb-1">
-				  <div class="avatar bg-light-success my-0 ms-0 me-50">
-					<span class="avatar-content">HJ</span>
-				  </div>
-				  <div class="more-info">
-					<p class="mb-0"><span class="fw-bold">Jordan</span> Left the board.</p>
-					<small class="text-muted">Today 11:00 AM</small>
-				  </div>
-				</div>
-				<div class="d-flex align-items-start mb-1">
-				  <div class="avatar my-0 ms-0 me-50">
-					<img src="{{asset('images/portrait/small/avatar-s-6.jpg')}}" alt="Avatar" height="32" />
-				  </div>
-				  <div class="more-info">
-					<p class="mb-0">
-					  <span class="fw-bold">Dianna</span> mentioned <span class="fw-bold text-primary">@bruce</span> in a
-					  comment.
-					</p>
-					<small class="text-muted">Today 10:20 AM</small>
-				  </div>
-				</div>
-				<div class="d-flex align-items-start mb-1">
-				  <div class="avatar my-0 ms-0 me-50">
-					<img src="{{asset('images/portrait/small/avatar-s-2.jpg')}}" alt="Avatar" height="32" />
-				  </div>
-				  <div class="more-info">
-					<p class="mb-0">
-					  <span class="fw-bold">Martian</span> added moved Charts & Maps task to the done board.
-					</p>
-					<small class="text-muted">Today 10:00 AM</small>
-				  </div>
-				</div>
-				<div class="d-flex align-items-start mb-1">
-				  <div class="avatar my-0 ms-0 me-50">
-					<img src="{{asset('images/portrait/small/avatar-s-1.jpg')}}" alt="Avatar" height="32" />
-				  </div>
-				  <div class="more-info">
-					<p class="mb-0"><span class="fw-bold">Barry</span> Commented on App review task.</p>
-					<small class="text-muted">Today 8:32 AM</small>
-				  </div>
-				</div>
-				<div class="d-flex align-items-start mb-1">
-				  <div class="avatar bg-light-dark my-0 ms-0 me-50">
-					<span class="avatar-content">BW</span>
-				  </div>
-				  <div class="more-info">
-					<p class="mb-0"><span class="fw-bold">Bruce</span> was assigned task of code review.</p>
-					<small class="text-muted">Today 8:30 PM</small>
-				  </div>
-				</div>
-				<div class="d-flex align-items-start mb-1">
-				  <div class="avatar bg-light-danger my-0 ms-0 me-50">
-					<span class="avatar-content">CK</span>
-				  </div>
-				  <div class="more-info">
-					<p class="mb-0">
-					  <span class="fw-bold">Clark</span> assigned task UX Research to
-					  <span class="fw-bold text-primary">@martian</span>
-					</p>
-					<small class="text-muted">Today 8:00 AM</small>
-				  </div>
-				</div>
-				<div class="d-flex align-items-start mb-1">
-				  <div class="avatar my-0 ms-0 me-50">
-					<img src="{{asset('images/portrait/small/avatar-s-4.jpg')}}" alt="Avatar" height="32" />
-				  </div>
-				  <div class="more-info">
-					<p class="mb-0">
-					  <span class="fw-bold">Ray</span> Added moved <span class="fw-bold">Forms & Tables</span> task from
-					  in progress to done.
-					</p>
-					<small class="text-muted">Today 7:45 AM</small>
-				  </div>
-				</div>
-				<div class="d-flex align-items-start mb-1">
-				  <div class="avatar my-0 ms-0 me-50">
-					<img src="{{asset('images/portrait/small/avatar-s-1.jpg')}}" alt="Avatar" height="32" />
-				  </div>
-				  <div class="more-info">
-					<p class="mb-0"><span class="fw-bold">Barry</span> Complete all the tasks assigned to him.</p>
-					<small class="text-muted">Today 7:17 AM</small>
-				  </div>
-				</div>
-				<div class="d-flex align-items-start mb-1">
-				  <div class="avatar bg-light-success my-0 ms-0 me-50">
-					<span class="avatar-content">HJ</span>
-				  </div>
-				  <div class="more-info">
-					<p class="mb-0"><span class="fw-bold">Jordan</span> added task to update new images.</p>
-					<small class="text-muted">Today 7:00 AM</small>
-				  </div>
-				</div>
-				<div class="d-flex align-items-start mb-1">
-				  <div class="avatar my-0 ms-0 me-50">
-					<img src="{{asset('images/portrait/small/avatar-s-6.jpg')}}" alt="Avatar" height="32" />
-				  </div>
-				  <div class="more-info">
-					<p class="mb-0">
-					  <span class="fw-bold">Dianna</span> moved task <span class="fw-bold">FAQ UX</span> from in progress
-					  to done board.
-					</p>
-					<small class="text-muted">Today 7:00 AM</small>
-				  </div>
-				</div>
-				<div class="d-flex align-items-start mb-1">
-				  <div class="avatar bg-light-danger my-0 ms-0 me-50">
-					<span class="avatar-content">CK</span>
-				  </div>
-				  <div class="more-info">
-					<p class="mb-0">
-					  <span class="fw-bold">Clark</span> added new board with name <span class="fw-bold">Done</span>.
-					</p>
-					<small class="text-muted">Yesterday 3:00 PM</small>
-				  </div>
-				</div>
-				<div class="d-flex align-items-start">
-				  <div class="avatar bg-light-dark my-0 ms-0 me-50">
-					<span class="avatar-content">BW</span>
-				  </div>
-				  <div class="more-info">
-					<p class="mb-0"><span class="fw-bold">Bruce</span> added new task in progress board.</p>
-					<small class="text-muted">Yesterday 12:00 PM</small>
-				  </div>
-				</div>
-			  </div>
-			</div>
-		  </div>
-		</div>
-	  </div>
-	</div>
-	<!-- Kanban Sidebar ends -->
-  </section>
-  <!-- Kanban ends -->
+                    <form id="addProjectForm" class="row gy-1 pt-75" action="{{ route('add.project') }}" method="POST">
+                        @csrf
+                        <div class="col-12 col-md-12">
+                            <label class="form-label" for="modalAddProjectName">Project Name</label>
+                            <input type="text" id="modalAddProjectName" name="modalAddProjectName"
+                                class="form-control" placeholder="Project Name" value=""
+                                data-msg="Please enter your project name" />
+                            <span id="error-modalAddProjectName" style="color: red; display: none"></span>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <label class="form-label" for="select2-modalAddPM">Project Manager</label>
+                            <select disabled name="modalAddPM" class="select2 form-select" id="select2-modalAddPM">
+                            </select>
+                            <span id="error-modalAddPM" style="color: red; display: none"></span>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <label class="form-label" for="select2-modalAddSupervisor">Project Supervisor</label>
+                            <select name="modalAddSupervisor" class="select2 form-select"
+                                id="select2-modalAddSupervisor">
+                            </select>
+                            <span id="error-modalAddSupervisor" style="color: red; display: none"></span>
+                        </div>
+                        <!-- Limit Selected Options -->
+                        <div class="col-12 col-md-12">
+                            <label class="form-label" for="select2-limited">Project Member</label>
+                            <select name="modalAddMembers[]" class="max-length form-select" id="select2-limited"
+                                multiple>
+                            </select>
+                            <span id="error-modalAddMembers" style="color: red; display: none"></span>
+                        </div>
+                        <div class="col-12 col-md-12">
+                            <label class="form-label" for="fp-range">Pick your project duration</label>
+                            <input name="duration" type="text" id="fp-range" class="form-control flatpickr-range"
+                                placeholder="YYYY-MM-DD to YYYY-MM-DD" />
+                            <span id="error-duration" style="color: red; display: none"></span>
+                        </div>
+                        <div class="col-12">
+                            <label class="form-label" for="modalAddDesc">Description</label>
+                            <textarea id="modalAddDesc" name="modalAddDesc" class="form-control" value=""
+                                placeholder="Enter project description"></textarea>
+                        </div>
+                        <div class="col-12 text-center mt-2 pt-50">
+                            <button style="display: none" id="spinnerBtnProject"
+                                class="btn btn-outline-primary waves-effect" type="button" disabled="">
+                                <span class="spinner-border spinner-border-sm" role="status"
+                                    aria-hidden="true"></span>
+                                <span class="ms-25 align-middle">Loading...</span>
+                            </button>
+                            <button type="submit" id="submitBtnProject" class="btn btn-primary me-1">Submit</button>
+                            <button type="reset" id="resetBtnProject" class="btn btn-outline-secondary"
+                                data-bs-dismiss="modal" aria-label="Close">
+                                Discard
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Kanban Sidebar ends -->
+</section>
+<!-- Kanban ends -->
