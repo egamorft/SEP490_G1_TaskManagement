8cea7f60c706a44bfd448acfceff2d889a943ad4
Trần Ngọc Hiếu <hieutnhe15199@fpt.edu.vn>
2023-07-10 20:16:06 +0700

    update master layout

--- app/Providers/PermissionProjectProvider.php
+++ app/Providers/PermissionProjectProvider.php
@@ -46,7 +46,6 @@ class PermissionProjectProvider extends ServiceProvider
                         ->where('role_id', $role_id)
                         ->pluck('permission_id')
                         ->toArray();
-						return true;
                     return in_array($permission->id, $permissions);
                 }
             }
