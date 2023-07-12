214556eeb7c0df1ad675675308c3329beae28d3d
Trần Ngọc Hiếu <hieutnhe15199@fpt.edu.vn>
2023-07-12 18:43:14 +0700

    add gantt chart

--- resources/views/project/gantt.blade.php
+++ resources/views/project/gantt.blade.php
@@ -1,35 +1,36 @@
 @extends('layouts/contentLayoutMaster')
 
 @section('content')
-	@include('project.header')
-	@include('task.gantt')
+    @include('project.header')
+    <div class="gantt"></div>
 @endsection
 
 @section('vendor-style')
-  <!-- Vendor css files -->
-  <link rel="stylesheet" href="{{ asset(mix('vendors/css/calendars/fullcalendar.min.css')) }}">
-  <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/select/select2.min.css')) }}">
-  <link rel="stylesheet" href="{{ asset(mix('vendors/css/pickers/flatpickr/flatpickr.min.css')) }}">
+    <!-- Vendor css files -->
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/calendars/fullcalendar.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/select/select2.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/pickers/flatpickr/flatpickr.min.css')) }}">
 @endsection
 
 @section('page-style')
-  <!-- Page css files -->
-  <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/pickers/form-flat-pickr.css')) }}">
-  <link rel="stylesheet" href="{{ asset(mix('css/base/pages/app-calendar.css')) }}">
-  <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
+    <!-- Page css files -->
+    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/pickers/form-flat-pickr.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/base/pages/app-calendar.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
 @endsection
 
 @section('vendor-script')
-  <!-- Vendor js files -->
-  <script src="{{ asset(mix('vendors/js/calendar/fullcalendar.min.js')) }}"></script>
-  <script src="{{ asset(mix('vendors/js/extensions/moment.min.js')) }}"></script>
-  <script src="{{ asset(mix('vendors/js/forms/select/select2.full.min.js')) }}"></script>
-  <script src="{{ asset(mix('vendors/js/forms/validation/jquery.validate.min.js')) }}"></script>
-  <script src="{{ asset(mix('vendors/js/pickers/flatpickr/flatpickr.min.js')) }}"></script>
+    <!-- Vendor js files -->
+    <script src="{{ asset(mix('vendors/js/calendar/fullcalendar.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/extensions/moment.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/forms/select/select2.full.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/forms/validation/jquery.validate.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/pickers/flatpickr/flatpickr.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/jquery/jquery.min.js')) }}"></script>
 @endsection
 
 @section('page-script')
-  <!-- Page js files -->
-  <script src="{{ asset(mix('js/scripts/pages/app-calendar-events.js')) }}"></script>
-  <script src="{{ asset(mix('js/scripts/pages/app-calendar.js')) }}"></script>
+    <!-- Page js files -->
+    <script src="{{ asset(mix('js/scripts/pages/task-jquery.fn.gantt.js')) }}"></script>
+    <script src="{{ asset(mix('js/scripts/pages/task-dataDays.js')) }}"></script>
 @endsection
