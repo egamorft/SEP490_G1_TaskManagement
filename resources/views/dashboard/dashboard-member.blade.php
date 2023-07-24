2e3b50e4965c36482ef814648d5ba723743e6176
Trần Ngọc Hiếu <hieutnhe15199@fpt.edu.vn>
2023-07-24 17:36:30 +0700

    update admin dashboard

--- resources/views/dashboard/dashboard-member.blade.php
+++ resources/views/dashboard/dashboard-member.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/contentLayoutMaster')
 
-@section('title', 'Dashboard Ecommerce')
+@section('title', 'Dashboard')
 
 @section('vendor-style')
     {{-- vendor css files --}}
@@ -24,116 +24,12 @@
 
     <!-- Dashboard Ecommerce Starts -->
     <section id="dashboard-ecommerce">
-        <div class="row match-height">
-            <!-- Medal Card -->
-            <div class="col-xl-4 col-md-6 col-12">
-                <div class="card card-congratulation-medal">
-                    <div class="card-body">
-                        <h5>Congratulations 🎉 John!</h5>
-                        <p class="card-text font-small-3">You have won gold medal</p>
-                        <h3 class="mb-75 mt-2 pt-50">
-                            <a href="#">$48.9k</a>
-                        </h3>
-                        <button type="button" class="btn btn-primary">View Sales</button>
-                        <img src="{{ asset('images/illustration/badge.svg') }}" class="congratulation-medal"
-                            alt="Medal Pic" />
-                    </div>
-                </div>
-            </div>
-            <!--/ Medal Card -->
-
-            <!-- Statistics Card -->
-            <div class="col-xl-8 col-md-6 col-12">
-                <div class="card card-statistics">
-                    <div class="card-header">
-                        <h4 class="card-title">Statistics</h4>
-                        <div class="d-flex align-items-center">
-                            <p class="card-text font-small-2 me-25 mb-0">Updated 1 month ago</p>
-                        </div>
-                    </div>
-                    <div class="card-body statistics-body">
-                        <div class="row">
-                            <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-0">
-                                <div class="d-flex flex-row">
-                                    <div class="avatar bg-light-primary me-2">
-                                        <div class="avatar-content">
-                                            <i data-feather="trending-up" class="avatar-icon"></i>
-                                        </div>
-                                    </div>
-                                    <div class="my-auto">
-                                        <h4 class="fw-bolder mb-0">230k</h4>
-                                        <p class="card-text font-small-3 mb-0">Sales</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-0">
-                                <div class="d-flex flex-row">
-                                    <div class="avatar bg-light-info me-2">
-                                        <div class="avatar-content">
-                                            <i data-feather="user" class="avatar-icon"></i>
-                                        </div>
-                                    </div>
-                                    <div class="my-auto">
-                                        <h4 class="fw-bolder mb-0">8.549k</h4>
-                                        <p class="card-text font-small-3 mb-0">Customers</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-sm-0">
-                                <div class="d-flex flex-row">
-                                    <div class="avatar bg-light-danger me-2">
-                                        <div class="avatar-content">
-                                            <i data-feather="box" class="avatar-icon"></i>
-                                        </div>
-                                    </div>
-                                    <div class="my-auto">
-                                        <h4 class="fw-bolder mb-0">1.423k</h4>
-                                        <p class="card-text font-small-3 mb-0">Products</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-sm-6 col-12">
-                                <div class="d-flex flex-row">
-                                    <div class="avatar bg-light-success me-2">
-                                        <div class="avatar-content">
-                                            <i data-feather="dollar-sign" class="avatar-icon"></i>
-                                        </div>
-                                    </div>
-                                    <div class="my-auto">
-                                        <h4 class="fw-bolder mb-0">$9745</h4>
-                                        <p class="card-text font-small-3 mb-0">Revenue</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!--/ Statistics Card -->
+		<div class="row match-height">
+			@include('dashboard.dashboard-related-project')
         </div>
-
-        <div class="row match-height">
-            <div class="col-lg-4 col-12">
-                <!-- Donut Chart Starts-->
-				<div class="card">
-					<div class="card-header flex-column align-items-start">
-						<h4 class="card-title mb-75">Task Overview</h4>
-						<span class="card-subtitle text-muted">Task breakdown by statuses </span>
-					</div>
-					<div class="card-body">
-						<div id="donut-chart"></div>
-					</div>
-                </div>
-                <!-- Donut Chart Ends-->
-            </div>
-
-            <!-- Revenue Report Card -->
-            <div class="col-lg-8 col-12">
-                @include('dashboard.calendar')
-            </div>
-            <!--/ Revenue Report Card -->
+		<div class="row match-height">
+			@include('dashboard.dashboard-related-task')
         </div>
-
     </section>
     <!-- Dashboard Ecommerce ends -->
 @endsection
---
+++ resources/views/dashboard/dashboard-related-project.blade.php
@@ -0,0 +1,192 @@
+	<!-- Project table -->
+	<div class="card col-12">
+		<h4 class="card-header">Your Related Projects</h4>
+		<div class="table-responsive">
+			<table class="table datatable-project">
+				<thead>
+					<tr>
+						<th>Project</th>
+						<th>Status</th>
+						<th>Progress</th>
+						<th>Your Role</th>
+						<th>Your Task</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="odd">
+						<td>
+							<div class="d-flex justify-content-left align-items-center">
+								<div class="more-info">
+									<h6 class="mb-0 text-danger"><a href="{{ route('view.project.board', ['slug'=>'mine']) }}">Project 1</a></h6>
+									<small>Project desc ...</small>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="d-flex justify-content-left align-items-center">
+								<div class="avatar float-start bg-light-danger rounded me-1">
+									<div class="avatar-content">
+										<i data-feather="alert-triangle" class="avatar-icon font-medium-3"></i>
+									</div>
+								</div>
+								<div class="more-info">
+									<h6 class="mb-0 text-danger">False</h6>
+									<small>You have fail this project according to supervisor proposed</small>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="more-info">
+								<p class="mb-50">Duration: {{ 20 }}% @if (10 > 0)
+									({{ 10 }} days remaining) 
+									@endif
+									@if (10 == 0)
+									(last day in the project) 
+									@endif
+								</p>
+								<div class="progress progress-bar-secondary" style="height: 6px">
+									<div class="progress-bar progress-bar-striped" role="progressbar"
+										aria-valuenow="{{ 20 }}" aria-valuemin="{{ 20 }}"
+										aria-valuemax="100"
+										style="width: {{ 20 }}%; ; background-color: {{ '#45ba30' }}!important">
+									</div>
+								</div>
+							</div>
+						</td>
+						<td>Project Manager</td>
+						<td>120/200</td>
+					</tr>
+					<tr class="even">
+						<td>
+							<div class="d-flex justify-content-left align-items-center">
+								<div class="more-info">
+									<h6 class="mb-0 text-danger"><a href="{{ route('view.project.board', ['slug'=>'mine']) }}">Project 2</a></h6>
+									<small>Project desc ...</small>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="d-flex justify-content-left align-items-center">
+								<div class="avatar float-start bg-light-warning rounded me-1">
+									<div class="avatar-content">
+										<i data-feather="pause-circle" class="avatar-icon font-medium-3"></i>
+									</div>
+								</div>
+								<div class="more-info">
+									<h6 class="mb-0 text-warning">Todo</h6>
+									<small>The project is not have supervisor</small>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="more-info">
+								<p class="mb-50">Duration: {{ 20 }}% @if (10 > 0)
+									({{ 10 }} days remaining) 
+									@endif
+									@if (10 == 0)
+									(last day in the project) 
+									@endif
+								</p>
+								<div class="progress progress-bar-secondary" style="height: 6px">
+									<div class="progress-bar progress-bar-striped" role="progressbar"
+										aria-valuenow="{{ 20 }}" aria-valuemin="{{ 20 }}"
+										aria-valuemax="100"
+										style="width: {{ 20 }}%; ; background-color: {{ '#45ba30' }}!important">
+									</div>
+								</div>
+							</div>
+						</td>
+						<td>Project Supervisor</td>
+						<td>120/200</td>
+					</tr>
+					<tr class="odd">
+						<td>
+							<div class="d-flex justify-content-left align-items-center">
+								<div class="more-info">
+									<h6 class="mb-0 text-danger"><a href="{{ route('view.project.board', ['slug'=>'mine']) }}">Project 3</a></h6>
+									<small>Project desc ...</small>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="d-flex justify-content-left align-items-center">
+								<div class="avatar float-start bg-light-primary rounded me-1">
+									<div class="avatar-content">
+										<i data-feather="fast-forward" class="avatar-icon font-medium-3"></i>
+									</div>
+								</div>
+								<div class="more-info">
+									<h6 class="mb-0 text-primary">Doing</h6>
+									<small>The project is in progress</small>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="more-info">
+								<p class="mb-50">Duration: {{ 20 }}% @if (10 > 0)
+									({{ 10 }} days remaining) 
+									@endif
+									@if (10 == 0)
+									(last day in the project) 
+									@endif
+								</p>
+								<div class="progress progress-bar-secondary" style="height: 6px">
+									<div class="progress-bar progress-bar-striped" role="progressbar"
+										aria-valuenow="{{ 20 }}" aria-valuemin="{{ 20 }}"
+										aria-valuemax="100"
+										style="width: {{ 20 }}%; ; background-color: {{ '#db8223' }}!important">
+									</div>
+								</div>
+							</div>
+						</td>
+						<td>Project Member</td>
+						<td>120/200</td>
+					</tr>
+					<tr class="odd">
+						<td>
+							<div class="d-flex justify-content-left align-items-center">
+								<div class="more-info">
+									<h6 class="mb-0 text-danger"><a href="{{ route('view.project.board', ['slug'=>'mine']) }}">Project 4</a></h6>
+									<small>Project desc ...</small>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="d-flex justify-content-left align-items-center">
+								<div class="avatar float-start bg-light-success rounded me-1">
+									<div class="avatar-content">
+										<i data-feather="check" class="avatar-icon font-medium-3"></i>
+									</div>
+								</div>
+								<div class="more-info">
+									<h6 class="mb-0 text-success">Done</h6>
+									<small>Congratulations!! You have done this project</small>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="more-info">
+								<p class="mb-50">Duration: {{ 20 }}% @if (10 > 0)
+									({{ 10 }} days remaining) 
+									@endif
+									@if (10 == 0)
+									(last day in the project) 
+									@endif
+								</p>
+								<div class="progress progress-bar-secondary" style="height: 6px">
+									<div class="progress-bar progress-bar-striped" role="progressbar"
+										aria-valuenow="{{ 20 }}" aria-valuemin="{{ 20 }}"
+										aria-valuemax="100"
+										style="width: {{ 20 }}%; ; background-color: {{ '#c4bc21' }}!important">
+									</div>
+								</div>
+							</div>
+						</td>
+						<td>Project Manager</td>
+						<td>120/200</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	<!-- /Project table -->
