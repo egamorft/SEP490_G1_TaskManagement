b1d0312f085e8339233f38d702f079345727919d
egamorft <huytung.htbn@gmail.com>
2023-08-02 13:14:52 +0700

    fix gantt

--- app/Http/Controllers/Api/GanttController.php
+++ app/Http/Controllers/Api/GanttController.php
@@ -14,8 +14,11 @@ class GanttController extends Controller
     {
         $tasksData = [];
         $tasks = Task::where('status', '<>', '0')
-            ->whereNull('deleted_at')
-            ->get();
+            ->whereNull('deleted_at')->whereHas('taskList.board.project', function ($query) use ($id) {
+                $query->where('id', $id);
+            })->get();
+
+        $linksData = $this->getTaskDependencies($tasks);
 
         $project = Project::findOrFail($id);
 
@@ -23,37 +26,67 @@ class GanttController extends Controller
         $startProject = new DateTime($project->start_date);
         $endProject = new DateTime($project->end_date);
         $projectData = [
-            'id' => 1,
+            'id' => 9999,
             'text' => $project->name,
             'readonly' => true,
-            'start_date' => $project->start_date,
+            'start_date' => \Carbon\Carbon::parse($project->start_date)->format('Y-m-d H:i:s'),
             'duration' => $startProject->diff($endProject)->days + 1,
-            'open' => true,
+            'parent' => 0,
             'type' => 'project',
         ];
         $tasksData[] = $projectData;
         // dd($tasksData);
 
-        $id = 2;
-        foreach ($tasks as $key => $task) {
+        foreach ($tasks as $task) {
             $startTask = new DateTime($task->start_date);
             $endTask = new DateTime($task->due_date);
             $data = [
-                'id' => $id,
+                'id' => $task->id,
                 'text' => $task->title,
-                'start_date' => $task->start_date,
+                'start_date' => \Carbon\Carbon::parse($task->start_date)->format('Y-m-d H:i:s'),
                 'duration' => $startTask->diff($endTask)->days + 1,
-                'parent' => 1,
+                'parent' => 9999,
                 'progress' => 1,
                 'status' => $task->status
             ];
             $tasksData[] = $data;
-            $id++;
         }
+
+        // dd($tasksData);
 
         return response()->json([
             "data" => $tasksData,
-            // "links" => $links
+            "links" => $linksData
         ]);
+    }
+
+    public function getTaskDependencies($tasks)
+    {
+        $dependencies = [];
+
+        // Loop through each task and its dependencies
+        $index = 1;
+        foreach ($tasks as $currentTask) {
+            $prevTasks = json_decode($currentTask->prev_tasks);
+
+            // Check if $prevTasks is not null and is an array or object
+            if (!is_null($prevTasks) && (is_array($prevTasks) || is_object($prevTasks))) {
+                foreach ($prevTasks as $prevTaskId) {
+                    $prevTask = $tasks->where('id', $prevTaskId)->first();
+                    $dependencies[] = [
+                        'id' => (string) $index,
+                        'source' => (string) $prevTask->id,
+                        'target' => (string) $currentTask->id,
+                        'type' => '0'
+                    ];
+                    $index++;
+                    // // Recursively add the dependencies of the previous task
+                    // $prevTaskDependencies = $this->getTaskDependencies($tasks);
+                    // $dependencies = array_merge($dependencies, $prevTaskDependencies);
+                }
+            }
+        }
+
+        return $dependencies;
     }
 }
