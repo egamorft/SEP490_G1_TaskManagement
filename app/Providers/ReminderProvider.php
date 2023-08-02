10d0353c628f540d46b463d1c0a221ee443614d7
vietphan <vietpthe150767@fpt.edu.vn>
2023-08-02 23:02:10 +0700

    Update reminder

--- app/Providers/ReminderProvider.php
+++ app/Providers/ReminderProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use App\Enums\TaskStatus;
 use App\Models\Board;
+use App\Models\Project;
 use App\Models\Task;
+use App\Models\TaskList;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
@@ -48,6 +50,79 @@ class ReminderProvider extends ServiceProvider
                                         ->where("due_date", ">=", strtotime("midnight", time()))
                                         ->where("due_date", "<=", strtotime("tomorrow", time())-1)
                                         ->get();
+                
+                // 
+                $taskList_ids = [];
+                foreach ($tasksReminder as $task) {
+                    $taskList_ids[] = $task->taskList_id;
+                }
+
+                $taskList_ids = array_unique($taskList_ids);
+                $taskLists = TaskList::whereIn("id", $taskList_ids)->get();
+
+                $board_ids = [];
+                foreach($taskLists as $taskList) {
+                    $board_ids[] = $taskList->board_id;
+                }
+                $board_ids = array_unique($board_ids);
+                $boards = Board::whereIn("id", $board_ids)->get();
+
+                $project_ids = [];
+                foreach ($boards as $board) {
+                    $project_ids[] = $board->project_id;
+                }
+                $projects = Project::whereIn("id", $project_ids)->get();
+
+                $boardProjects = [];
+                foreach($projects as $project) {
+                    $boardProjects[$project->id] = $project;
+                }
+
+                $taskListBoards = [];
+                foreach ($boards as $board) {
+                    if (!isset($boardProjects[$board->project_id])) {
+                        continue;
+                    }
+                    $board->project = $boardProjects[$board->project_id];
+                    $taskListBoards[$board->id] = $board;
+                }
+
+                $taskInTaskLists = [];
+                foreach ($taskLists as $taskList) {
+                    if (!isset($taskListBoards[$taskList->board_id])) {
+                        continue;
+                    }
+                    $taskList->board = $taskListBoards[$taskList->board_id];
+                    $taskList->project = $taskList->board->project;
+                    $taskInTaskLists[$taskList->id] = $taskList;
+                }
+
+                foreach($tasksReminder as $task) {
+                    if (!isset($taskInTaskLists[$task->taskList_id])) {
+                        continue;
+                    }
+                    $task->taskList = $taskInTaskLists[$task->taskList_id];
+                    $task->board = $task->taskList->board;
+                    $task->project = $task->taskList->project;
+                }
+
+                foreach($tasksLateReminder as $task) {
+                    if (!isset($taskInTaskLists[$task->taskList_id])) {
+                        continue;
+                    }
+                    $task->taskList = $taskInTaskLists[$task->taskList_id];
+                    $task->board = $task->taskList->board;
+                    $task->project = $task->taskList->project;
+                }
+
+                foreach($tasksTodayReminder as $task) {
+                    if (!isset($taskInTaskLists[$task->taskList_id])) {
+                        continue;
+                    }
+                    $task->taskList = $taskInTaskLists[$task->taskList_id];
+                    $task->board = $task->taskList->board;
+                    $task->project = $task->taskList->project;
+                }
                 
                 $view->with(compact(
                     'tasksReminder',
--- resources/views/panels/reminderLayout.blade.php
+++ resources/views/panels/reminderLayout.blade.php
@@ -88,7 +88,7 @@
 
                     <div class="reminder-content">
                         <div class="reminder-title">
-                            <a href="{{ route('task.modalsDetail', ['slug' => $project->slug, 'board_id' => $board->id, 'task_id' => $taskRemind->id]) }}">{{ $taskRemind->title }}</a>
+                            <a href="{{ route('task.modalsDetail', ['slug' => $taskRemind->project->slug, 'board_id' => $taskRemind->board->id, 'task_id' => $taskRemind->id]) }}">{{ $taskRemind->title }}</a>
                         </div>
                         <div class="reminder-sub-title {{ $statusView["class"] }}">
                             <div class="reminder-date-input">{{ date("d/m/Y", strtotime($taskRemind->due_date)) }}</div>
@@ -161,7 +161,7 @@
 
                     <div class="reminder-content">
                         <div class="reminder-title">
-                            <a href="{{ route('task.modalsDetail', ['slug' => $project->slug, 'board_id' => $board->id, 'task_id' => $taskRemind->id]) }}">{{ $taskRemind->title }}</a>
+                            <a href="{{ route('task.modalsDetail', ['slug' => $taskRemind->project->slug, 'board_id' => $taskRemind->board->id, 'task_id' => $taskRemind->id]) }}">{{ $taskRemind->title }}</a>
                         </div>
                         <div class="reminder-sub-title {{ $statusView["class"] }}">
                             <div class="reminder-date-input">{{ date("d/m/Y", strtotime($taskRemind->due_date)) }}</div>
