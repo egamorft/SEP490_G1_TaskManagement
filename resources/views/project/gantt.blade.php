2698a0995355ec71594a654cd1b381be44953418
egamorft <huytung.htbn@gmail.com>
2023-08-02 23:55:08 +0700

    fix gantt for fixed start date project and ignore linked task duplicate

--- resources/views/project/gantt.blade.php
+++ resources/views/project/gantt.blade.php
@@ -8,6 +8,8 @@
 
 @section('vendor-style')
     <!-- Vendor css files -->
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/gantt/dhtmlxgantt_material.css')) }}">
+    
     <link rel="stylesheet" href="{{ asset(mix('vendors/css/calendars/fullcalendar.min.css')) }}">
     <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/select/select2.min.css')) }}">
     <link rel="stylesheet" href="{{ asset(mix('vendors/css/pickers/flatpickr/flatpickr.min.css')) }}">
@@ -21,8 +23,23 @@
     <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
     <!-- Page js files -->
     {{-- <script src="{{ asset(mix('js/scripts/pages/task-gantt.js')) }}"></script> --}}
-    <script src="https://cdn.dhtmlx.com/gantt/edge/dhtmlxgantt.js"></script>
+    <script src="{{ asset(mix('vendors/js/gantt/dhtmlxgantt.js')) }}"></script>
+    {{-- <script src="https://cdn.dhtmlx.com/gantt/edge/dhtmlxgantt.js"></script> --}}
     <link href="https://cdn.dhtmlx.com/gantt/edge/dhtmlxgantt.css" rel="stylesheet">
+    <style>
+        
+		.task_groups {
+			background-color: orangered !important;
+		}
+
+		.task_groups .gantt_task_progress {
+			background-color: red;
+			opacity: 0.6;
+		}
+		.gantt_task_row.gantt_selected .weekend {
+			background-color: #C0E8FF !important;
+		}
+    </style>
 @endsection
 
 @section('vendor-script')
--- resources/views/task/gantt.blade.php
+++ resources/views/task/gantt.blade.php
@@ -33,7 +33,7 @@
     // gantt.config.order_branch_free = true; /*!*/
 
     gantt.plugins({
-		auto_scheduling: true,
+        auto_scheduling: true,
         marker: true
     });
     var dateToStr = gantt.date.date_to_str(gantt.config.task_date);
@@ -44,18 +44,127 @@
         text: "Today",
         title: "Today: " + dateToStr(today)
     });
+    
+	gantt.config.auto_scheduling = true;
+	gantt.config.auto_scheduling_strict = true;
 
-    var start = new Date('{{ $projectStartDateFormatted }}');
-    gantt.addMarker({
-        start_date: start,
-        css: "status_line",
-        text: "Start project",
-        title: "Start project: " + dateToStr(start)
-    });
+    (function() {
+        var highlightTasks = [],
+            highlightSearch = {};
+
+        function reset(value) {
+            if (value) {
+                if (value.join() === highlightTasks.join()) {
+                    return;
+                }
+                highlightTasks = value;
+                highlightSearch = {};
+                highlightTasks.forEach(function(id) {
+                    highlightSearch[id] = true;
+                });
+                gantt.render();
+            } else if (highlightTasks.length) {
+                highlightTasks = [];
+                highlightSearch = {};
+                gantt.render();
+            }
+        }
+
+        gantt.templates.task_class = function(start, end, task) {
+            if (highlightSearch[task.id])
+                return "task_groups";
+            return "";
+        };
+
+        gantt.attachEvent("onTaskClick", function(id) {
+            var task = gantt.getTask(id);
+            var group = gantt.getConnectedGroup(id);
+            if (!group.tasks.length) {
+                reset();
+            } else {
+                reset(group.tasks);
+                gantt.message({
+                    text: "<strong>Selected task:</strong> " + task.text +
+                        "<br><strong>Connected Group:</strong><br> " +
+                        group.tasks.map(function(t) {
+                            return gantt.getTask(t).text
+                        }).join("<br>"),
+                    expire: 5000
+                });
+            }
+
+            return true;
+        });
+
+        gantt.attachEvent("onEmptyClick", function() {
+            reset();
+            return true;
+        });
+
+    })();
+
+	gantt.message({text:"Click any task to highlight the connected group", expire:-1});
 
     gantt.init("gantt_here");
     gantt.load("/api/data/{{ $project->id }}");
 
+    function limitMoveLeft(task, limit) {
+        var dur = task.end_date - task.start_date;
+        task.end_date = new Date(limit.end_date);
+        task.start_date = new Date(+task.end_date - dur);
+    }
+
+    function limitMoveRight(task, limit) {
+        var dur = task.end_date - task.start_date;
+        task.start_date = new Date(limit.start_date);
+        task.end_date = new Date(+task.start_date + dur);
+    }
+
+    function limitResizeLeft(task, limit) {
+        task.end_date = new Date(limit.end_date);
+    }
+
+    function limitResizeRight(task, limit) {
+        task.start_date = new Date(limit.start_date)
+    }
+    gantt.attachEvent("onTaskDrag", function(id, mode, task, original, e) {
+        var parent = task.parent ? gantt.getTask(task.parent) : null,
+            children = gantt.getChildren(id),
+            modes = gantt.config.drag_mode;
+
+        var limitLeft = null,
+            limitRight = null;
+
+        if (!(mode == modes.move || mode == modes.resize)) return;
+
+        if (mode == modes.move) {
+            limitLeft = limitMoveLeft;
+            limitRight = limitMoveRight;
+        } else if (mode == modes.resize) {
+            limitLeft = limitResizeLeft;
+            limitRight = limitResizeRight;
+        }
+
+        //check parents constraints
+        if (parent && +parent.end_date < +task.end_date) {
+            limitLeft(task, parent);
+        }
+        if (parent && +parent.start_date > +task.start_date) {
+            limitRight(task, parent);
+        }
+
+        //check children constraints
+        for (var i = 0; i < children.length; i++) {
+            var child = gantt.getTask(children[i]);
+            if (+task.end_date < +child.end_date) {
+                limitLeft(task, child);
+            } else if (+task.start_date > +child.start_date) {
+                limitRight(task, child)
+            }
+        }
+
+
+    });
     // // Gantt Size
     // var start_gantt = "{{ $ganttStartDate }}";
     // var end_gantt = "{{ $ganttEndDate }}";
