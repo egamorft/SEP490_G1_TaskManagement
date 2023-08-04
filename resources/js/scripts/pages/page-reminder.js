29ee051bf0eb50c066dc86ff922a2a12d4a0d49d
Viet Phan <phantuanviet0706@gmail.com>
2023-08-04 20:53:47 +0700

    Update reminder

--- resources/js/scripts/pages/page-reminder.js
+++ resources/js/scripts/pages/page-reminder.js
@@ -1,27 +1,29 @@
-$(window).on("load", function() {
-        var reminderButton = $('#reminder-canvas-wrapper'),
-            reminderSection = $('#reminder-canvas'),
-            removeReminderSection = $('.reminder-close');
+$(window).on("load", function () {
+	var reminderButton = $('.reminder-button-wrapper'),
+		reminderSection = $('#reminder-canvas'),
+		removeReminderSection = $('#reminder-canvas .reminder-close');
 
-        
-        reminderButton.on("click", function() {
-            if(reminderSection.hasClass("hidden")) {
-                reminderSection.removeClass("hidden");
-            } else {
-                reminderSection.addClass("hidden");
-            }
-        });
 
-        $('.tabs .reminder-tab').on("click", function() {
-            $('.reminder-tab.active').removeClass("active");
-            $('.reminder-body .tab.active').removeClass("active");
+	reminderButton.on("click", function () {
+		if (reminderSection.hasClass("hidden")) {
+			reminderSection.removeClass("hidden");
+		} else {
+			reminderSection.addClass("hidden");
+		}
+	});
 
-            $(this).addClass("active");
-            var dataTab = $(this).attr("data-tab");
-            $(`.reminder-body .tab-${dataTab}`).addClass("active");
-        });
+	$('.tabs .reminder-tab').on("click", function () {
+		$('.reminder-tab.active').removeClass("active");
+		$('.reminder-body .tab.active').removeClass("active");
 
-        removeReminderSection.on("click", function() {
-            $(this).closest("#reminder-canvas").addClass("hidden");
-        });
+		$(this).addClass("active");
+		var dataTab = $(this).attr("data-tab");
+		$(`.reminder-body .tab-${dataTab}`).addClass("active");
+	});
+
+	removeReminderSection.on("click", function () {
+		$(this).closest("#reminder-canvas").addClass("hidden");
+	});
+
+
 });
